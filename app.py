cae148d54153b518c606736c17a3707e82e4660a
Panakj Jovian <pankaj@jovian.com>
2023-06-22 17:27:42 +0530

    add comment

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 # importing webdriver
+# w3
 from selenium import webdriver
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
