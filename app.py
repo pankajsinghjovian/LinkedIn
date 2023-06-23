0de4f84d126b987de813d74250d9af1c4568b52d
Panakj Jovian <pankaj@jovian.com>
2023-06-23 19:56:34 +0530

    changed the user agent back to normal

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ from selenium import webdriver
 
 # Waiting  for an element to be visible before performing an action
 
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'
+# user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'
+user_agent ='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.37'
+
 
 # Setting up the chromium options 
 def set_chrome() -> Options:
@@ -88,7 +90,7 @@ def scrape_jobs():
         
         # # Showing the images
         # image.show()
-        image_path='./image.png'
+        image_path='.image.png'
 
 
 
